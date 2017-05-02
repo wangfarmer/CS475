c5399fddf9cbacc0fa81abc2596fb05bc21232d3
dannyquint <dannyquint@gmail.com>
2017-05-02 19:22:12 -0400

    compute updated but no longer used

--- src/compute/Compute.java
+++ src/compute/Compute.java
@@ -3,8 +3,9 @@ package compute;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import compute.Task;
+import java.math.BigDecimal;
 
 public interface Compute extends Remote {
-      <T> T executeTask(Task<T> t) throws RemoteException;
-      <T> T retrieveInfo(Task<T> t) throws RemoteException;
+      BigDecimal executeTask(int digits) throws RemoteException;
+      BigDecimal retrieveInfo(Pi t) throws RemoteException;
 }
