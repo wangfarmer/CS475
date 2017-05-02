d067d3f2e37886e7db1da53308e8a6ea6a1bb7ed
dannyquint <dannyquint@gmail.com>
2017-05-02 19:22:45 -0400

    updated how jar is built to new infrastructure

--- src/start.sh
+++ src/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-javac compute/Compute.java compute/Task.java
+javac compute/Pi.java
 jar cvf compute.jar compute/*.class
 cp compute.jar public_html/classes/
-javac -cp public_html/classes/compute.jar engine/ComputeEngine.java
+javac -cp public_html/classes/compute.jar engine/PiImpl.java
 
-javac -cp /home/dannyquint/git/CS475/src/public_html/classes/compute.jar client/ComputePi.java client/Pi.java
-cp client/Pi.class public_html/classes/client/
+javac -cp /home/dannyquint/git/CS475/src/public_html/classes/compute.jar client/ComputePi.java engine/PiImpl.java
+cp engine/PiImpl.class public_html/classes/client/
 rmiregistry &
-java -cp /home/dannyquint/git/CS475/src/:/home/dannyquint/git/CS475/src/public_html/classes/compute.jar -Djava.rmi.server.codebase=file:./home/dannyquint/git/CS475/src/public_html/classes/ -Djava.security.policy=server.policy engine.ComputeEngine dan &
+java -cp /home/dannyquint/git/CS475/src/:/home/dannyquint/git/CS475/src/public_html/classes/compute.jar -Djava.rmi.server.codebase=file:./home/dannyquint/git/CS475/src/public_html/classes/ -Djava.security.policy=server.policy engine.PiImpl dan &
 java -cp /home/dannyquint/git/CS475/src/:/home/dannyquint/git/CS475/src/public_html/classes/compute.jar -Djava.rmi.server.codebase=file:./home/dannyquint/git/CS475/src/public_html/classes/ -Djava.security.policy=client.policy client.ComputePi 127.0.0.1 dan retrieve-event 4
